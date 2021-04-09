63d37cf5201c52c09ea594425a5d9970430d888a
Hisham Assi <assi.hisham@gmail.com>
2021-04-09 16:30:28 -0400

    Update get_osensa_transmitter.m

--- get_osensa_transmitter.m
+++ get_osensa_transmitter.m
@@ -1,7 +1,7 @@
 
 function[transmitter]= get_osensa_transmitter(port)
-ospy=py.importlib.import_module('osensAssi');
+ospy=py.importlib.import_module('osensapy');
 transmitter = ospy.Transmitter(port, uint16(247));
- disp("Osensa Transmitter is ON")
+disp("Osensa Transmitter is ON")
 
 
